9d2769a96d822584544cb118e1e701d5013da711
Andrew Antles <103275315+andrewantles@users.noreply.github.com>
2024-04-23 15:11:13 -0700

    Fix typo - add dev vpc

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-I think Rapalo are default
+
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
