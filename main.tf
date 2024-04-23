bf5a135fb47676b2576d971089044cb83c0e8dff
Andrew Antles <103275315+andrewantles@users.noreply.github.com>
2024-04-23 16:11:20 -0700

    Missing comma

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ module "alb" {
       port        = 80
       target_type = "instance"
       targets = {
-        target_id = aws_instance.blog.id
+        target_id = aws_instance.blog.id,
         port      = 80
       }
     }
