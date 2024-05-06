15c068645557e99cc3f76c03e857154a1cd1ded4
Andrew Antles <103275315+andrewantles@users.noreply.github.com>
2024-05-06 16:06:29 -0700

    match tutorial alb code exactly

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+# TODO: should be able to delete this
 data "aws_vpc" "default" {
   default = true
 }
@@ -25,7 +26,6 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -49,19 +49,22 @@ resource "aws_instance" "blog" {
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
+  load_balancer_type = "application"
+
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  security_groups = module.blog_sg.security_group_id
   
   target_groups = [
     {
-      name_prefix = "blog-"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
       targets = {
         my_target = {
           target_id = aws_instance.blog.id
@@ -71,7 +74,7 @@ module "alb" {
     }
   ]
 
-  listeners = [
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
